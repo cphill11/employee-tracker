af988484a334b539c1ca6c9bb7ce7644536be34e
Criste Phillips <criste.phillips@yahoo.com>
2022-02-13 09:03:55 -0500

    tables started, employee table fails

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (10,2) NOT NULL,
-  department_id INTEGER NOT NULL,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  department_id INTEGER,
+  fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -23,7 +23,7 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   -- hold reference to employee role
   role_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     -- make NULL if employee has no manager (HOW?)
   manager_id INTEGER,
   CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO parties (name, id)
+INSERT INTO department (id, name)
 VALUES
-('Accounting', 1),
-('Marketing', 2),
-('Human Resources', 3)
+(1, 'Accounting'),
+(2, 'Marketing'),
+(3, 'Human Resources')
 
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES
 -- how to make salary into a usable amount (???)
-    (5.1, Manager, 'Salary', 3),
-    (5.2, Slavelabor, 'free', 1),
-    (5.3, Salesman, 'Way too much', 2)
+    (4, 'Manager', 50000, 3),
+    (3, 'Assistant', 30000, 1),
+    (2, 'Salesman', 35000, 2)
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-    (5.1, Bertha, Jorkins, 1.1),
-    (5.2, Mathilda, Bagshot, 2.2),
-    (5.3, Wilber, Wilkinson, 3.3)
+    (51, 'Bertha', 'Jorkins', 4, 2),
+    (52, 'Mathilda', 'Bagshot', 3, 1),
+    (53, 'Wilber', 'Wilkinson', 2, 1)
