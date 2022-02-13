e3ebab197c22be4d1c1ae340789c0c30024a51cb
Criste Phillips <criste.phillips@yahoo.com>
2022-02-13 13:43:43 -0500

    first two tables join appropriately

--- db/schema.sql
+++ db/schema.sql
@@ -14,17 +14,18 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (10,2) NOT NULL,
   department_id INTEGER,
-  fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  -- hold reference to employee role
-  role_id INTEGER,
-  fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    -- make NULL if employee has no manager (HOW?)
-  manager_id INTEGER,
-  CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-);
+-- first 2 tables work
+-- CREATE TABLE employee (
+--   id INTEGER PRIMARY KEY,
+--   first_name VARCHAR(30) NOT NULL,
+--   last_name VARCHAR(30) NOT NULL,
+--   -- hold reference to employee role
+--   role_id INTEGER,
+--   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+--     -- make NULL if employee has no manager (HOW?)
+--   manager_id INTEGER
+-- --   CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+-- );
